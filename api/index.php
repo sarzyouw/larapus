815e383045d5b7572f5a9e8d936694ae40526812
unknown <queensarahh23@gmail.com>
2026-01-27 10:56:51 +0700

    Membungkam pesan deprecated di entry point

--- api/index.php
+++ api/index.php
@@ -1,12 +1,19 @@
 <?php
-// Mengarahkan ke file autoload Laravel 5.3
+
+// 1. Sembunyikan pesan Deprecated agar tidak menutupi tampilan
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
+ini_set('display_errors', '0');
+
+// 2. Load aplikasi Laravel
 require __DIR__ . '/../bootstrap/autoload.php';
-// Mengarahkan ke bootstrap app
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
+
 $response->send();
+
 $kernel->terminate($request, $response);
